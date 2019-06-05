a7f0bc56015e4831957a32a1125279e3efb90613
Quang Kha <quangkhait98@gmail.com>
2019-06-05 11:28:52 +0700

    storage
    kha

--- app/src/main/java/com/example/socialnetwork/Fragment/Fragment_NewFeed.java
+++ app/src/main/java/com/example/socialnetwork/Fragment/Fragment_NewFeed.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.OpenableColumns;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,20 +22,23 @@ import android.widget.Toast;
 
 import com.example.socialnetwork.Objects.Post;
 import com.example.socialnetwork.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.io.File;
-
-import static android.content.ContentValues.TAG;
-
+import java.io.ByteArrayOutputStream;
 public class Fragment_NewFeed extends Fragment {
     View view;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference mDatabase = database.getReference("Post");
     EditText et_status;
     Button btnPush;
-
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReferenceFromUrl("gs://social-network-14488.appspot.com");
     ImageView imageView;
     Button btnimage;
     private static final int PICK_IMAGE=100;
@@ -46,6 +51,7 @@ public class Fragment_NewFeed extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_fragment__new_feed, container, false);
         et_status= view.findViewById(R.id.etStatus);
+
         btnPush=view.findViewById(R.id.btnGui);
         btnPush.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,15 +71,17 @@ public class Fragment_NewFeed extends Fragment {
     }
 
 
-    public void pushPost(){
-        String status=et_status.getText().toString();
-        Post post= new Post();
+    public void pushPost() {
+        String status = et_status.getText().toString();
+        Post post = new Post();
         post.setAccount_name("Quan");
         post.setText(status);
-        String id= mDatabase.push().getKey();
+        String id = mDatabase.push().getKey();
         post.setPost_id(id);
         mDatabase.child(id).setValue(post);
+        uploadimage();
 
+    }
     private void opengallery()
     {
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
@@ -90,8 +98,36 @@ public class Fragment_NewFeed extends Fragment {
             Cursor cursor = getActivity().getContentResolver().query(imageUri, null, null, null, null);
             int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
             cursor.moveToFirst();
-            //Toast.makeText(getContext(),cursor.getString(nameIndex),Toast.LENGTH_LONG).show();
+            Toast.makeText(getContext(),cursor.getString(nameIndex),Toast.LENGTH_LONG).show();
             imagename= cursor.getString(nameIndex);
         }
+    }
+    public void uploadimage()
+    {
+
+        imageView.setDrawingCacheEnabled(true);
+        imageView.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] data = baos.toByteArray();
+        StorageReference mountainsRef = storageRef.child(imagename);
+        UploadTask uploadTask = mountainsRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+                Toast.makeText(getContext(),"loi ",Toast.LENGTH_LONG).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+                Toast.makeText(getContext(),"thanh cong ",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
