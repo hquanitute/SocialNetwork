13b7b1ebd0fab912b2b1539fb27afeaf95b575cc
Hoang Quan <32866869+hquanitute@users.noreply.github.com>
2019-06-05 10:27:05 +0700

    Sua post bai

--- app/src/main/java/com/example/socialnetwork/Fragment/Fragment_NewFeed.java
+++ app/src/main/java/com/example/socialnetwork/Fragment/Fragment_NewFeed.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.socialnetwork.Objects.Post;
@@ -14,13 +15,34 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Fragment_NewFeed extends Fragment {
     View view;
-
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference mDatabase = database.getReference("Post");
+    EditText et_status;
+    Button btnPush;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_fragment__new_feed, container, false);
+        et_status= view.findViewById(R.id.etStatus);
+        btnPush=view.findViewById(R.id.btnGui);
+        btnPush.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pushPost();
+            }
+        });
         return view;
+    }
+
+    public void pushPost(){
+        String status=et_status.getText().toString();
+        Post post= new Post();
+        post.setAccount_name("Quan");
+        post.setText(status);
+        String id= mDatabase.push().getKey();
+        post.setPost_id(id);
+        mDatabase.child(id).setValue(post);
     }
 }
